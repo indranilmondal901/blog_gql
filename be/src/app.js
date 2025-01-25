173c2da87a297d22ee01716f5fc793915f9320e2
indranilmondal901 <122368872+indranilmondal901@users.noreply.github.com>
2025-01-25 19:25:08 +0530

    #path update

--- be/src/app.js
+++ be/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const postSchema = require('./schemas/postSchema.js');
-const resolvers = require('./resolvers/postResolvers.js');
+const postSchema = require('../src/schemas/postSchema.js');
+const resolvers = require('../src/resolvers/postResolvers.js');
 const cors = require('cors');
 require('dotenv').config();
 
